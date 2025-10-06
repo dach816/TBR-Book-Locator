f50c244fdeebcdc5e0a190cd87e82cd44e36ef57
Sarah D <sdachinger@gmail.com>
2025-10-06 16:10:57 -0400

    Add dark mode and a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './components/Search';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Container, Row, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <h1 className='mb-5 mt-3' style={{ textAlign: 'center' }}>Book Locator</h1>
-      </Row>
-      <Search />
-    </Container>
+    <div data-bs-theme="dark">
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">TBR Book Locator</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container id='search-page'>
+        <Row>
+          <h1 className='mb-5 mt-3' style={{ textAlign: 'center', color: 'lightgray' }}>Book Locator</h1>
+        </Row>
+        <Search />
+      </Container>
+    </div>
   )
 }
 
