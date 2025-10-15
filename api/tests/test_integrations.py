6659e9a15c31064095b2f5dd845a16cc9c1f809e
Sarah D <sdachinger@gmail.com>
2025-10-15 17:48:56 -0400

    Update tests to use setup methods, separate tests into one assertion per test

--- api/tests/test_integrations.py
+++ api/tests/test_integrations.py
@@ -1,150 +1,182 @@
-from ..api import integrations
+import sys
+import os
+import pytest
 
-def test_everand_query(requests_mock):
-    isbn = "0062802402"
-    requests_mock.get(f"{integrations.everand_query_url}?query={isbn}", json={"results":{"audiobooks":{"content":{"documents":[{"book_preview_url":"url"}]}}}, "total_results_count": 1})
-    bookInfo = integrations.query_everand(isbn)
-    assert bookInfo.hasAudiobook == True
-    assert bookInfo.hasEbook == False
-    assert bookInfo.audiobookLinks == ["url"]
-    assert len(bookInfo.ebookLinks) == 0
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-def test_everand_data_audiobook():
-    # 1 audiobook link
-    json = {"results":{"audiobooks":{"content":{"documents":[{"book_preview_url":"url"}]}}}, "total_results_count": 1}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo.hasAudiobook == True
-    assert bookInfo.hasEbook == False
-    assert bookInfo.audiobookLinks == ["url"]
-    assert len(bookInfo.ebookLinks) == 0
+import integrations
 
-    # 0 audiobook links
-    json = {"results":{"audiobooks":{"content":{"documents":[]}}}, "total_results_count": 1}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo.hasAudiobook == False
-    assert bookInfo.hasEbook == False
-    assert len(bookInfo.audiobookLinks) == 0
-    assert len(bookInfo.ebookLinks) == 0
+class Test_Everand:
+    def setup_method(self):
+        AUDIOBOOK = "audiobooks"
+        EBOOK = "books"
+        bookLink = {"book_preview_url":"url"}
+        noneBookLink = {"book_preview_url":None}
 
-    # 2 audiobook links
-    json = {"results":{"audiobooks":{"content":{"documents":[{"book_preview_url":"url1"}, {"book_preview_url":"url2"}]}}}, "total_results_count": 1}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo.hasAudiobook == True
-    assert bookInfo.hasEbook == False
-    assert bookInfo.audiobookLinks == ["url1", "url2"]
-    assert len(bookInfo.ebookLinks) == 0
+        generateBooksByLinks = lambda bookType, links: {bookType:{"content":{"documents":links}}}
+        generateBooks = lambda bookType, numLinks: generateBooksByLinks(bookType, [bookLink] * numLinks)
+        generateResults = lambda bookType, numLinks: {"results":generateBooks(bookType, numLinks), "total_results_count":1}
 
-def test_everand_data_ebook():
-    # 1 ebook link
-    json = {"results":{"books":{"content":{"documents":[{"book_preview_url":"url"}]}}}, "total_results_count": 1}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo.hasAudiobook == False
-    assert bookInfo.hasEbook == True
-    assert len(bookInfo.audiobookLinks) == 0
-    assert bookInfo.ebookLinks == ["url"]
+        self.audiobookResultsNoLinks = generateResults(AUDIOBOOK, 0)
+        self.audiobookResultsOneLink = generateResults(AUDIOBOOK, 1)
+        self.audiobookResultsTwoLinks = generateResults(AUDIOBOOK, 2)
+        self.ebookResultsNoLinks = generateResults(EBOOK, 0)
+        self.ebookResultsOneLink = generateResults(EBOOK, 1)
+        self.ebookResultsTwoLinks = generateResults(EBOOK, 2)
 
-    # 0 ebook links
-    json = {"results":{"books":{"content":{"documents":[]}}}, "total_results_count": 1}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo.hasAudiobook == False
-    assert bookInfo.hasEbook == False
-    assert len(bookInfo.audiobookLinks) == 0
-    assert len(bookInfo.ebookLinks) == 0
+        self.audiobookNoLinks = generateBooks(AUDIOBOOK, 0)
+        self.audiobookOneLink = generateBooks(AUDIOBOOK, 1)
+        self.audiobookTwoLinks = generateBooks(AUDIOBOOK, 2)
+        self.audiobookTwoLinksOneNone = generateBooksByLinks(AUDIOBOOK, [bookLink, noneBookLink])
 
-    # 2 ebook links
-    json = {"results":{"books":{"content":{"documents":[{"book_preview_url":"url1"}, {"book_preview_url":"url2"}]}}}, "total_results_count": 1}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo.hasAudiobook == False
-    assert bookInfo.hasEbook == True
-    assert len(bookInfo.audiobookLinks) == 0
-    assert bookInfo.ebookLinks == ["url1", "url2"]
+        self.noResults = {"results":{}, "total_results_count": 0}
+        self.noAudiobookContent = {AUDIOBOOK:{}}
+        self.noAudiobookDocuments = {AUDIOBOOK:{"content":{}}}
+        self.audiobookLinksNone = generateBooksByLinks(AUDIOBOOK, [noneBookLink])
 
-def test_everand_data_none():
-    json = {"results":{}, "total_results_count": 0}
-    bookInfo = integrations.handle_everand_data(json)
-    assert bookInfo == None
+    def test_everand_query(self, requests_mock):
+        isbn = "0062802402"
+        requests_mock.get(f"{integrations.everand_query_url}?query={isbn}", json=self.audiobookResultsOneLink)
+        bookInfo = integrations.query_everand(isbn)
+        assert bookInfo.hasAudiobook == True
+        assert bookInfo.hasEbook == False
+        assert bookInfo.audiobookLinks == ["url"]
+        assert len(bookInfo.ebookLinks) == 0
 
-def test_everand_links_not_found():
-    # Empty results
-    links = integrations.get_everand_book_links({}, "test")
-    assert len(links) == 0
+    def test_everand_data_audiobook_one_link(self):
+        bookInfo = integrations.handle_everand_data(self.audiobookResultsOneLink)
+        assert bookInfo.hasAudiobook == True
+        assert bookInfo.hasEbook == False
+        assert bookInfo.audiobookLinks == ["url"]
+        assert len(bookInfo.ebookLinks) == 0
 
-    # Book type not in json
-    json = {"audiobooks":{"content":{"documents":[{"book_preview_url":"url"}]}}}
-    links = integrations.get_everand_book_links(json, "test")
-    assert len(links) == 0
+    def test_everand_data_audiobook_no_links(self):
+        bookInfo = integrations.handle_everand_data(self.audiobookResultsNoLinks)
+        assert bookInfo.hasAudiobook == False
+        assert bookInfo.hasEbook == False
+        assert len(bookInfo.audiobookLinks) == 0
+        assert len(bookInfo.ebookLinks) == 0
 
-    # content not in json
-    json = {"audiobooks":{"test":{}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert len(links) == 0
+    def test_everand_data_audiobook_two_links(self):
+        bookInfo = integrations.handle_everand_data(self.audiobookResultsTwoLinks)
+        assert bookInfo.hasAudiobook == True
+        assert bookInfo.hasEbook == False
+        assert bookInfo.audiobookLinks == ["url", "url"]
+        assert len(bookInfo.ebookLinks) == 0
 
-    # documents not in json
-    json = {"audiobooks":{"content":{"test":[]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert len(links) == 0
+    def test_everand_data_ebook_one_link(self):
+        bookInfo = integrations.handle_everand_data(self.ebookResultsOneLink)
+        assert bookInfo.hasAudiobook == False
+        assert bookInfo.hasEbook == True
+        assert len(bookInfo.audiobookLinks) == 0
+        assert bookInfo.ebookLinks == ["url"]
 
-    # Book url not in json
-    json = {"audiobooks":{"content":{"documents":[{"test":"url"}]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert len(links) == 0
+    def test_everand_data_ebook_no_links(self):
+        bookInfo = integrations.handle_everand_data(self.ebookResultsNoLinks)
+        assert bookInfo.hasAudiobook == False
+        assert bookInfo.hasEbook == False
+        assert len(bookInfo.audiobookLinks) == 0
+        assert len(bookInfo.ebookLinks) == 0
 
-    # Empty documents
-    json = {"audiobooks":{"content":{"documents":[]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert len(links) == 0
+    def test_everand_data_ebook_two_links(self):
+        bookInfo = integrations.handle_everand_data(self.ebookResultsTwoLinks)
+        assert bookInfo.hasAudiobook == False
+        assert bookInfo.hasEbook == True
+        assert len(bookInfo.audiobookLinks) == 0
+        assert bookInfo.ebookLinks == ["url", "url"]
 
-    # Book url is null
-    json = {"audiobooks":{"content":{"documents":[{"test":None}]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert len(links) == 0
+    def test_everand_data_none(self):
+        bookInfo = integrations.handle_everand_data(self.noResults)
+        assert bookInfo == None
 
-def test_everand_links_found():
-    json = {"audiobooks":{"content":{"documents":[{"book_preview_url":"url"}]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert links == ["url"]
+    def test_everand_links_not_found_no_results(self):
+        links = integrations.get_everand_book_links({}, "test")
+        assert len(links) == 0
+
+    def test_everand_links_not_found_wrong_book_type(self):
+        links = integrations.get_everand_book_links(self.audiobookOneLink, "test")
+        assert len(links) == 0
+
+    def test_everand_links_not_found_no_content(self):
+        links = integrations.get_everand_book_links(self.noAudiobookContent, "audiobooks")
+        assert len(links) == 0
+
+    def test_everand_links_not_found_no_documents(self):
+        links = integrations.get_everand_book_links(self.noAudiobookDocuments, "audiobooks")
+        assert len(links) == 0
+
+    def test_everand_links_not_found_no_links(self):
+        links = integrations.get_everand_book_links(self.audiobookNoLinks, "audiobooks")
+        assert len(links) == 0
+
+    def test_everand_links_not_found_link_url_none(self):
+        links = integrations.get_everand_book_links(self.audiobookLinksNone, "audiobooks")
+        assert len(links) == 0
+
+    def test_everand_links_found_one_link(self):
+        links = integrations.get_everand_book_links(self.audiobookOneLink, "audiobooks")
+        assert links == ["url"]
+        
+    def test_everand_links_found_two_links(self):
+        links = integrations.get_everand_book_links(self.audiobookTwoLinks, "audiobooks")
+        assert links == ["url", "url"]
+        
+    def test_everand_links_found_two_links_one_none(self):
+        links = integrations.get_everand_book_links(self.audiobookTwoLinksOneNone, "audiobooks")
+        assert links == ["url"]
+
+class Test_Hardcover:
+    def setup_method(self):
+        generateHardcoverData = lambda hasAudiobook, hasEbook: {"data": {"search": {"results": {"hits": [{"document": {"author_names": ["Author1, Author2"],"description": "Book description","has_audiobook": hasAudiobook,"has_ebook": hasEbook,"image": {"url": "image url"},"isbns": ["9781797113647","1534403019"],"pages": 496,"rating": 3.978260869565217,"release_date": "2018-01-01","slug": "hardcover-slug","title": "Book Title", "id": "ID"}}]}}}}
+        self.audiobookHardcoverData = generateHardcoverData(True, False)
+        self.ebookHardcoverData = generateHardcoverData(False, True)
+        self.noBookHardcoverData = generateHardcoverData(False, False)
+        self.noResults = {"data": {"search": {"results": {"hits": []}}}}
+
+    @pytest.fixture
+    def book(self):
+        bookDetails = integrations.handle_hardcover_data(self.audiobookHardcoverData)
+        return bookDetails[0]
     
-    json = {"audiobooks":{"content":{"documents":[{"book_preview_url":"url1"}, {"book_preview_url":"url2"}]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert links == ["url1", "url2"]
-    
-    json = {"audiobooks":{"content":{"documents":[{"book_preview_url":"url1"}, {"book_preview_url":None}]}}}
-    links = integrations.get_everand_book_links(json, "audiobooks")
-    assert links == ["url1"]
+    def test_hardcover_data_book_authors(self, book):
+        assert book.authors == "Author1, Author2"
 
-def test_hardcover_data():
-    # Get 1 good result
-    json = {"data": {"search": {"results": {"hits": [{"document": {"author_names": ["Author1, Author2"],"description": "Book description","has_audiobook": True,"has_ebook": True,"image": {"url": "image url"},"isbns": ["9781797113647","1534403019"],"pages": 496,"rating": 3.978260869565217,"release_date": "2018-01-01","slug": "hardcover-slug","title": "Book Title", "id": "ID"}}]}}}}
-    bookDetails = integrations.handle_hardcover_data(json)
-    assert len(bookDetails) == 1
+    def test_hardcover_data_book_image_url(self, book):
+        assert book.coverImageUrl == "image url"
 
-    book = bookDetails[0]
-    assert book.authors == "Author1, Author2"
-    assert book.coverImageUrl == "image url"
-    assert book.description == "Book description"
-    assert book.isbns == ["9781797113647","1534403019"]
-    assert book.pages == 496
-    assert book.rating == 3.978260869565217
-    assert book.releaseDate == "2018-01-01"
-    assert book.title == "Book Title"
-    assert book.url == f"{integrations.hardcover_book_url}hardcover-slug"
+    def test_hardcover_data_book_description(self, book):
+        assert book.description == "Book description"
 
-    # Get 1 good result (has_ebook false)
-    json = {"data": {"search": {"results": {"hits": [{"document": {"author_names": ["Author1, Author2"],"description": "Book description","has_audiobook": True,"has_ebook": False,"image": {"url": "image url"},"isbns": ["9781797113647","1534403019"],"pages": 496,"rating": 3.978260869565217,"release_date": "2018-01-01","slug": "hardcover-slug","title": "Book Title", "id": "ID"}}]}}}}
-    bookDetails = integrations.handle_hardcover_data(json)
-    assert len(bookDetails) == 1
+    def test_hardcover_data_book_isbns(self, book):
+        assert book.isbns == ["9781797113647","1534403019"]
 
-    # Get 1 good result (has_audiobook false)
-    json = {"data": {"search": {"results": {"hits": [{"document": {"author_names": ["Author1, Author2"],"description": "Book description","has_audiobook": False,"has_ebook": True,"image": {"url": "image url"},"isbns": ["9781797113647","1534403019"],"pages": 496,"rating": 3.978260869565217,"release_date": "2018-01-01","slug": "hardcover-slug","title": "Book Title", "id": "ID"}}]}}}}
-    bookDetails = integrations.handle_hardcover_data(json)
-    assert len(bookDetails) == 1
+    def test_hardcover_data_book_pages(self, book):
+        assert book.pages == 496
 
-    # Get 1 bad result
-    json = {"data": {"search": {"results": {"hits": [{"document": {"author_names": ["Author1, Author2"],"description": "Book description","has_audiobook": False,"has_ebook": False,"image": {"url": "image url"},"isbns": ["9781797113647","1534403019"],"pages": 496,"rating": 3.978260869565217,"release_date": "2018-01-01","slug": "hardcover-slug","title": "Book Title", "id": "ID"}}]}}}}
-    bookDetails = integrations.handle_hardcover_data(json)
-    assert len(bookDetails) == 0
+    def test_hardcover_data_book_rating(self, book):
+        assert book.rating == 3.978260869565217
 
-    # Get no results
-    json = {"data": {"search": {"results": {"hits": []}}}}
-    bookDetails = integrations.handle_hardcover_data(json)
-    assert len(bookDetails) == 0
+    def test_hardcover_data_book_release_date(self, book):
+        assert book.releaseDate == "2018-01-01"
+
+    def test_hardcover_data_book_title(self, book):
+        assert book.title == "Book Title"
+
+    def test_hardcover_data_hardcover_url(self, book):
+        assert book.url == f"{integrations.hardcover_book_url}hardcover-slug"
+
+    def test_hardcover_audiobook_results_size(self):
+        bookDetails = integrations.handle_hardcover_data(self.audiobookHardcoverData)
+        assert len(bookDetails) == 1
+
+    def test_hardcover_ebook_results_size(self):
+        bookDetails = integrations.handle_hardcover_data(self.ebookHardcoverData)
+        assert len(bookDetails) == 1
+
+    def test_hardcover_no_audiobook_no_ebook(self):
+        bookDetails = integrations.handle_hardcover_data(self.noBookHardcoverData)
+        assert len(bookDetails) == 0
+
+    def test_hardcover_no_results(self):
+        bookDetails = integrations.handle_hardcover_data(self.noResults)
+        assert len(bookDetails) == 0
